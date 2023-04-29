36fb017b16427c7947bc0ab22c46cdf3632cf294
Nesquiko <lukas.castven@gmail.com>
2023-04-29 12:50:48 +0200

    Bench with cloning

--- benches/big_data.rs
+++ benches/big_data.rs
@@ -4,119 +4,87 @@ use rust_project_fiit_stu::{dll, immutable_ll, immutable_thread_safe_ll, ll};
 criterion_group!(big_data_ops, bench_push, bench_pop, bench_get);
 criterion_main!(big_data_ops);
 
-const N: usize = 1_000;
-const M: usize = 100;
+const N: usize = 100;
+const M: usize = 10_000;
 
 fn bench_push(c: &mut Criterion) {
     let mut group = c.benchmark_group("BigData-Push");
+    group.sampling_mode(SamplingMode::Flat);
 
     let mut big_arr: [u64; M] = [0; M];
     for i in 0..M {
         big_arr[i] = i as u64;
     }
-    let mut big_arrs = [big_arr; N];
-
-    group.sampling_mode(SamplingMode::Flat);
 
     group.bench_function("Vec", |b| {
         b.iter_batched(
-            move || {
-                for i in 0..N {
-                    big_arrs[i] = big_arr.clone();
-                }
-                Vec::new()
-            },
+            || Vec::new(),
             |mut v| {
-                for arr in big_arrs {
-                    v.push(arr);
+                for _ in 0..N {
+                    v.push(big_arr.clone());
                 }
             },
-            BatchSize::PerIteration,
+            BatchSize::SmallInput,
         );
     });
 
     group.bench_function("Std LL", |b| {
         b.iter_batched(
-            move || {
-                for i in 0..N {
-                    big_arrs[i] = big_arr.clone();
-                }
-                std::collections::LinkedList::new()
-            },
+            || std::collections::LinkedList::new(),
             |mut std_ll| {
-                for arr in big_arrs {
-                    std_ll.push_front(arr);
+                for _ in 0..N {
+                    std_ll.push_front(big_arr.clone());
                 }
             },
-            BatchSize::PerIteration,
+            BatchSize::SmallInput,
         );
     });
 
     group.bench_function("LL", |b| {
         b.iter_batched(
-            move || {
-                for i in 0..N {
-                    big_arrs[i] = big_arr.clone();
-                }
-                ll::LinkedList::new()
-            },
+            || ll::LinkedList::new(),
             |mut ll| {
-                for arr in big_arrs {
-                    ll.push(arr);
+                for _ in 0..N {
+                    ll.push(big_arr.clone());
                 }
             },
-            BatchSize::PerIteration,
+            BatchSize::SmallInput,
         );
     });
 
     group.bench_function("Immutable LL", |b| {
         b.iter_batched(
-            move || {
-                for i in 0..N {
-                    big_arrs[i] = big_arr.clone();
-                }
-                immutable_ll::ImmutableLinkedList::new()
-            },
+            || immutable_ll::ImmutableLinkedList::new(),
             |mut immutable_ll| {
-                for arr in big_arrs {
-                    immutable_ll = immutable_ll.prepend(arr);
+                for _ in 0..N {
+                    immutable_ll = immutable_ll.prepend(big_arr.clone());
                 }
             },
-            BatchSize::PerIteration,
+            BatchSize::SmallInput,
         );
     });
 
     group.bench_function("Thread Safe LL", |b| {
         b.iter_batched(
-            move || {
-                for i in 0..N {
-                    big_arrs[i] = big_arr.clone();
-                }
-                immutable_thread_safe_ll::ImmutableLinkedList::new()
-            },
+            || immutable_thread_safe_ll::ImmutableLinkedList::new(),
             |mut immutable_thread_safe_ll| {
-                for arr in big_arrs {
-                    immutable_thread_safe_ll = immutable_thread_safe_ll.prepend(arr);
+                for _ in 0..N {
+                    immutable_thread_safe_ll = immutable_thread_safe_ll.prepend(big_arr.clone());
                 }
             },
-            BatchSize::PerIteration,
+            BatchSize::SmallInput,
         );
     });
 
     group.bench_function("DLL", |b| {
         b.iter_batched(
-            move || {
-                for i in 0..N {
-                    big_arrs[i] = big_arr.clone();
-                }
-                dll::DoublyLinkedList::new()
-            },
+            || dll::DoublyLinkedList::new(),
             |mut dll| {
-                for arr in big_arrs {
-                    dll.push_front(arr);
+                for _ in 0..N {
+                    dll.push_front(big_arr.clone());
                 }
             },
-            BatchSize::PerIteration,
+            BatchSize::SmallInput,
         );
     });
 }
@@ -145,7 +113,7 @@ fn bench_pop(c: &mut Criterion) {
                     blackhole = v.pop();
                 }
             },
-            BatchSize::PerIteration,
+            BatchSize::SmallInput,
         );
     });
 
@@ -163,7 +131,7 @@ fn bench_pop(c: &mut Criterion) {
                     blackhole = std_ll.pop_front();
                 }
             },
-            BatchSize::PerIteration,
+            BatchSize::SmallInput,
         );
     });
 
@@ -181,7 +149,7 @@ fn bench_pop(c: &mut Criterion) {
                     blackhole = ll.pop();
                 }
             },
-            BatchSize::PerIteration,
+            BatchSize::SmallInput,
         );
     });
 
@@ -199,7 +167,7 @@ fn bench_pop(c: &mut Criterion) {
                     immutable_ll = immutable_ll.tail();
                 }
             },
-            BatchSize::PerIteration,
+            BatchSize::SmallInput,
         );
     });
 
@@ -218,7 +186,7 @@ fn bench_pop(c: &mut Criterion) {
                     immutable_thread_safe_ll = immutable_thread_safe_ll.tail();
                 }
             },
-            BatchSize::PerIteration,
+            BatchSize::SmallInput,
         );
     });
 
@@ -236,7 +204,7 @@ fn bench_pop(c: &mut Criterion) {
                     blackhole = dll.pop_front();
                 }
             },
-            BatchSize::PerIteration,
+            BatchSize::SmallInput,
         );
     });
 }
